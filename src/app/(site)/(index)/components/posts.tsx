83fc3b9b1d80dcf2ab3df05927ca9b14d5a05849
Mehmet <mehmetext68@gmail.com>
2024-12-18 13:02:59 +0300

    Update posts.tsx

--- src/app/(site)/(index)/components/posts.tsx
+++ src/app/(site)/(index)/components/posts.tsx
@@ -27,14 +27,13 @@ const DUMMY_CATEGORY: Category = {
 };
 
 export default function Posts() {
-  const ITEMS_PER_PAGE = 6;
   const TOTAL_PAGES = 10; // Bu değer API'den gelmeli
 
   return (
     <div className="space-y-6">
       <H1>Yazılar</H1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {Array.from({ length: ITEMS_PER_PAGE }).map((_, i) => (
+        {Array.from({ length: 6 }).map((_, i) => (
           <PostItem key={i} post={DUMMY_POST} category={DUMMY_CATEGORY} />
         ))}
       </div>
