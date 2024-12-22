c6818ce2cd64d23631a6b23935ca609d47664db8
Mehmet <mehmetext68@gmail.com>
2024-12-22 10:22:51 +0300

    feat: update actions

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -2,12 +2,46 @@
 
 import { Category, Comment, Post, User } from "@prisma/client";
 
-async function fetchAPI<T>(endpoint: string): Promise<T> {
+type RequestConfig = {
+  method?: string;
+  body?: unknown;
+  headers?: HeadersInit;
+  cache?: RequestCache;
+  next?: NextFetchRequestConfig;
+};
+
+async function fetchAPI<T>(
+  endpoint: string,
+  config: RequestConfig = {}
+): Promise<T> {
+  const { method = "GET", body, headers = {}, cache, next } = config;
+
   try {
-    const response = await fetch(`${process.env.API_URL}/api/${endpoint}`);
+    const requestHeaders: HeadersInit = {
+      "Content-Type": "application/json",
+      ...headers,
+    };
+
+    const requestConfig: RequestInit = {
+      method,
+      headers: requestHeaders,
+      cache,
+      next,
+    };
+
+    if (body) {
+      requestConfig.body = JSON.stringify(body);
+    }
+
+    const response = await fetch(
+      `${process.env.API_URL}/api/${endpoint}`,
+      requestConfig
+    );
+
     if (!response.ok) {
       throw new Error(`API error: ${response.statusText}`);
     }
+
     const data = await response.json();
     return data.data;
   } catch (error) {
@@ -16,30 +50,23 @@ async function fetchAPI<T>(endpoint: string): Promise<T> {
   }
 }
 
-export async function getPosts() {
-  return fetchAPI<
-    (Post & {
-      category: Category;
-      author: User;
-      comments: Comment[];
-    })[]
-  >("posts");
-}
+export const getPosts = fetchAPI<
+  (Post & {
+    category: Category;
+    author: User;
+    comments: Comment[];
+  })[]
+>("posts");
 
-export async function getPost(slug: string) {
-  return fetchAPI<
-    Post & {
-      category: Category;
-      author: User;
-      comments: Comment[];
-    }
-  >(`posts/${slug}`);
-}
+export const getPost = fetchAPI<
+  Post & {
+    category: Category;
+    author: User;
+    comments: Comment[];
+  }
+>("posts/${slug}");
 
-export async function getCategories() {
-  return fetchAPI<Category[]>("categories");
-}
+export const getCategories = fetchAPI<Category[]>("categories");
 
-export async function getCategory(slug: string) {
-  return fetchAPI<Category>(`categories/${slug}`);
-}
+export const getCategory = (slug: string) =>
+  fetchAPI<Category>(`categories/${slug}`);
