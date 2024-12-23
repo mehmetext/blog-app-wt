f466bbf36fb8bdbd8b0c7871cc954f7d426c567d
Mehmet <mehmetext68@gmail.com>
2024-12-23 13:40:16 +0300

    feat: add some components for data-table

--- src/app/admin/posts/posts-columns.tsx
+++ src/app/admin/posts/posts-columns.tsx
@@ -2,6 +2,7 @@
 
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import { DataTableColumnHeader } from "@/components/ui/data-table-header";
 import { Category, Comment, Post, User } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
 import { format } from "date-fns";
@@ -11,7 +12,9 @@ import Link from "next/link";
 export default [
   {
     accessorKey: "title",
-    header: "Title",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Başlık" />
+    ),
     cell: ({ row }) => {
       return (
         <Link
@@ -26,42 +29,54 @@ export default [
   },
   {
     id: "category",
-    header: "Kategori",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Kategori" />
+    ),
     cell: ({ row }) => {
       return row.original.category.name;
     },
   },
   {
     accessorKey: "author",
-    header: "Yazar",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Yazar" />
+    ),
     cell: ({ row }) => {
       return row.original.author.name;
     },
   },
   {
     accessorKey: "comments",
-    header: "Yorumlar",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Yorumlar" />
+    ),
     cell: ({ row }) => {
       return row.original.comments.length;
     },
   },
   {
     accessorKey: "createdAt",
-    header: "Oluşturulma Tarihi",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Oluşturulma Tarihi" />
+    ),
     cell: ({ row }) => {
       return format(row.original.createdAt, "dd.MM.yyyy HH:mm");
     },
   },
   {
     accessorKey: "updatedAt",
-    header: "Güncellenme Tarihi",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Güncellenme Tarihi" />
+    ),
     cell: ({ row }) => {
       return format(row.original.updatedAt, "dd.MM.yyyy HH:mm");
     },
   },
   {
     accessorKey: "status",
-    header: "Durum",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Durum" />
+    ),
     cell: ({ row }) => {
       return row.original.deletedAt ? (
         <Badge variant="destructive">Silindi</Badge>
@@ -72,7 +87,10 @@ export default [
   },
   {
     accessorKey: "actions",
-    header: "İşlemler",
+    enableSorting: false,
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="İşlemler" />
+    ),
     cell: ({ row }) => {
       return (
         <Button
