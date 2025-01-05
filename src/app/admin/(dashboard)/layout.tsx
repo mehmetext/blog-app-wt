ce6958c6f3b056d9df483c9ebaeba85b2858eb6c
Mehmet <mehmetext68@gmail.com>
2025-01-05 21:17:13 +0300

    Update layout.tsx

--- src/app/admin/(dashboard)/layout.tsx
+++ src/app/admin/(dashboard)/layout.tsx
@@ -3,6 +3,8 @@ import { SidebarProvider } from "@/components/ui/sidebar";
 import { redirect } from "next/navigation";
 import Sidebar from "./components/sidebar";
 
+export const dynamic = "force-dynamic";
+
 export default async function AdminLayout({
   children,
 }: {
